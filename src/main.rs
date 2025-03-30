2eec339e865f86a408a0164922f61cdcf5984aba
Atul Varma <varmaa@gmail.com>
2025-03-30 11:55:06 -0400

    Add Node, Context.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,56 @@
 use std::{cell::RefCell, ops::Add, rc::Rc};
 
+#[derive(Debug, Clone, Copy)]
+pub struct NodeId(usize);
+
+pub enum Node {
+    Float(f64),
+    Sum(NodeId, NodeId),
+}
+
+#[derive(Default)]
+pub struct Context {
+    nodes: Vec<Node>,
+}
+
+impl Context {
+    fn new_node(&mut self, node: Node) -> NodeId {
+        let id = self.nodes.len();
+        self.nodes.push(node);
+        NodeId(id)
+    }
+
+    fn get_node(&self, node: NodeId) -> &Node {
+        self.nodes.get(node.0).expect("node id must exist")
+    }
+
+    fn get_node_mut(&mut self, node: NodeId) -> &mut Node {
+        self.nodes.get_mut(node.0).expect("node id must exist")
+    }
+
+    pub fn set(&mut self, node: NodeId, new_value: f64) {
+        let Node::Float(value) = self.get_node_mut(node) else {
+            panic!("node must be a float");
+        };
+        *value = new_value;
+    }
+
+    pub fn param(&mut self, value: f64) -> NodeId {
+        self.new_node(Node::Float(value))
+    }
+
+    pub fn sum(&mut self, a: NodeId, b: NodeId) -> NodeId {
+        self.new_node(Node::Sum(a, b))
+    }
+
+    pub fn compute(&self, node: NodeId) -> f64 {
+        match self.get_node(node) {
+            Node::Float(node) => *node,
+            Node::Sum(a, b) => self.compute(*a) + self.compute(*b),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Param(Rc<RefCell<f64>>);
 
@@ -62,4 +113,14 @@ fn main() {
     println!("{sum:?} = {}", sum.compute());
     x.set(2.0);
     println!("{sum:?} = {}", sum.compute());
+
+    {
+        let mut ctx = Context::default();
+        let x = ctx.param(1.0);
+        let y = ctx.param(3.0);
+        let sum = ctx.sum(x, y);
+        println!("{}", ctx.compute(sum));
+        ctx.set(x, 2.0);
+        println!("{}", ctx.compute(sum));
+    }
 }
