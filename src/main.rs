e9ecbeb03f353628370da720a8f03039577f8ce8
Atul Varma <varmaa@gmail.com>
2025-03-30 17:10:16 -0400

    Add backward, multiply.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ mod value;
 use value::Value;
 
 fn main() {
-    let x = Value::new_param("x", 1.0);
-    let sum = x + 3.0.into();
-    println!("{sum} = {}", sum.as_f64());
+    // This example is taken from Karpathy's lecture:
+    // https://www.youtube.com/watch?v=VMj-3S1tku0
+    let a = Value::new_param("a", 2.0);
+    let b = Value::new_param("b", -3.0);
+    let c = Value::new_param("c", 10.0);
+    let e = a * b;
+    let d = e + c;
+    let f = Value::new_param("f", -2.0);
+    let mut loss = d * f;
+    println!("{loss} = {}", loss.as_f64());
+    loss.backward();
+    println!("{loss:#?}");
 }
--- src/value.rs
+++ src/value.rs
@@ -1,4 +1,10 @@
-use std::{cell::RefCell, fmt::Display, ops::Add, rc::Rc};
+use std::{
+    cell::RefCell,
+    collections::HashSet,
+    fmt::Display,
+    ops::{Add, Mul},
+    rc::Rc,
+};
 
 #[derive(Debug, Clone)]
 pub struct Value(Rc<RefCell<InnerValue>>);
@@ -25,6 +31,54 @@ impl Value {
     pub fn new_param<T: AsRef<str>>(name: T, value: f64) -> Value {
         InnerValue::new(ValueType::Float(Some(name.as_ref().to_owned())), value).into()
     }
+
+    fn children(&self) -> Vec<Value> {
+        match &self.0.borrow()._type {
+            ValueType::Float(_) => vec![],
+            ValueType::Mul(a, b) => vec![a.clone(), b.clone()],
+            ValueType::Sum(a, b) => vec![a.clone(), b.clone()],
+        }
+    }
+
+    fn order_topologically(&self) -> Vec<Value> {
+        let visited: HashSet<i64> = HashSet::new();
+        let mut result = vec![];
+        let mut to_visit = vec![self.clone()];
+
+        while let Some(value) = to_visit.pop() {
+            let ptr = Rc::as_ptr(&value.0) as i64;
+            if visited.contains(&ptr) {
+                continue;
+            }
+            result.push(value.clone());
+            to_visit.extend(value.children());
+        }
+
+        result
+    }
+
+    pub fn backward(&mut self) {
+        self.0.borrow_mut().grad = 1.0;
+        let values = self.order_topologically();
+        for mut value in values {
+            value.local_backward();
+        }
+    }
+
+    fn local_backward(&mut self) {
+        let value = &self.0.borrow();
+        match &value._type {
+            ValueType::Float(_) => {}
+            ValueType::Sum(a, b) => {
+                a.0.borrow_mut().grad += value.grad;
+                b.0.borrow_mut().grad += value.grad;
+            }
+            ValueType::Mul(a, b) => {
+                a.0.borrow_mut().grad += b.0.borrow().value * value.grad;
+                b.0.borrow_mut().grad += a.0.borrow().value * value.grad;
+            }
+        }
+    }
 }
 
 impl Display for Value {
@@ -45,21 +99,39 @@ impl Add<Value> for Value {
     }
 }
 
+impl Mul<Value> for Value {
+    type Output = Value;
+
+    fn mul(self, rhs: Value) -> Self::Output {
+        InnerValue::new(
+            ValueType::Mul(self.clone(), rhs.clone()),
+            self.as_f64() * rhs.as_f64(),
+        )
+        .into()
+    }
+}
+
 #[derive(Debug)]
 enum ValueType {
     Float(Option<String>),
     Sum(Value, Value),
+    Mul(Value, Value),
 }
 
 #[derive(Debug)]
 struct InnerValue {
     _type: ValueType,
     value: f64,
+    grad: f64,
 }
 
 impl InnerValue {
     fn new(_type: ValueType, value: f64) -> Self {
-        InnerValue { _type, value }
+        InnerValue {
+            _type,
+            value,
+            grad: 0.0,
+        }
     }
 }
 
@@ -68,7 +140,8 @@ impl Display for InnerValue {
         match &self._type {
             ValueType::Float(Some(name)) => write!(f, "{name}"),
             ValueType::Float(None) => write!(f, "{}", self.value),
-            ValueType::Sum(a, b) => write!(f, "{} + {}", a, b),
+            ValueType::Sum(a, b) => write!(f, "({} + {})", a, b),
+            ValueType::Mul(a, b) => write!(f, "({} * {})", a, b),
         }
     }
 }
