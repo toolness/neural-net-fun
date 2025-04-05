d3b5508bf825e94153be4872b1b6bdaa636101b3
Atul Varma <varmaa@gmail.com>
2025-04-05 15:38:41 -0400

    Make neuron, activation functions.

--- src/perceptron.rs
+++ src/perceptron.rs
@@ -81,33 +81,45 @@ impl Perceptron {
     }
 }
 
+type ActivationFn = fn(value: Value) -> Value;
+
+fn sigmoid(value: Value) -> Value {
+    Value::from(1.0) / (Value::from(1.0) + (value * (-1.0).into()).exp())
+}
+
+fn neuron(weights: &Vec<Value>, inputs: &Vec<Value>, activation: ActivationFn) -> Value {
+    // The weights will have one extra for the bias.
+    assert_eq!(weights.len(), inputs.len() + 1);
+    let mut sum = weights.last().unwrap().clone();
+    for (weight, input) in weights.iter().zip(inputs) {
+        sum = sum + weight.clone() * input.clone();
+    }
+    activation(sum)
+}
+
 struct NeuralNet {
     loss: Value,
-    w1: Value,
-    w2: Value,
-    w3: Value,
+    weights: Vec<Value>,
 }
 
 impl NeuralNet {
     fn get_label(x: f64, y: f64, w: &Vec3) -> i32 {
-        let sum = w.0 + x * w.1 + y * w.2;
+        let sum = x * w.0 + y * w.1 + w.2;
         let sigmoid = sum.exp() / (1.0 + sum.exp());
         if sigmoid <= 0.5 { 0 } else { 1 }
     }
 
     fn new(points: &Vec<Datapoint>, w: &Vec3) -> Self {
-        let w1 = Value::from(w.0);
-        let w2 = Value::from(w.1);
-        let w3 = Value::from(w.2);
+        let weights = vec![Value::from(w.0), Value::from(w.1), Value::from(w.2)];
         let mut loss = Value::from(0.0);
         for point in points {
-            let x1 = Value::from(1.0);
-            let x2 = Value::from(point.pos.0 as f64);
-            let x3 = Value::from(point.pos.1 as f64);
-            let sum = x1 * w1.clone() + x2 * w2.clone() + x3 * w3.clone();
-            let sigmoid = Value::from(1.0) / (Value::from(1.0) + (sum * (-1.0).into()).exp());
+            let inputs = vec![
+                Value::from(point.pos.0 as f64),
+                Value::from(point.pos.1 as f64),
+            ];
+            let output = neuron(&weights, &inputs, sigmoid);
             let y = Value::from(point.label as f64);
-            let single_loss = (y - sigmoid.clone()).pow(2.0);
+            let single_loss = (y - output.clone()).pow(2.0);
             // println!(
             //     "{point:?}, sigmoid={:0.2} loss={:0.2}",
             //     sigmoid.as_f64(),
@@ -117,13 +129,17 @@ impl NeuralNet {
         }
         loss = loss / Value::from(points.len() as f64);
 
-        NeuralNet { loss, w1, w2, w3 }
+        NeuralNet { loss, weights }
     }
 
     fn into_grad(mut self) -> Vec3 {
         self.loss.backward();
-        if self.w1.grad().is_finite() && self.w2.grad().is_finite() && self.w3.grad().is_finite() {
-            let grad = Vec3(self.w1.grad(), self.w2.grad(), self.w3.grad());
+
+        let w3 = self.weights.pop().unwrap();
+        let w2 = self.weights.pop().unwrap();
+        let w1 = self.weights.pop().unwrap();
+        if w1.grad().is_finite() && w2.grad().is_finite() && w3.grad().is_finite() {
+            let grad = Vec3(w1.grad(), w2.grad(), w3.grad());
             grad
         } else {
             // TODO: Consider resetting any weight that isn't finite.
