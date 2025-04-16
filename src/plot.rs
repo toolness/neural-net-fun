52c79680ea7b90051c848ac271882bb45037bc10
Atul Varma <varmaa@gmail.com>
2025-04-15 22:26:08 -0400

    Make plot re-center on window resize.

--- src/plot.rs
+++ src/plot.rs
@@ -2,45 +2,49 @@ use macroquad::prelude::*;
 
 pub struct Plot {
     scale: f32,
-    origin: (f32, f32),
 }
 
 impl Plot {
     pub fn new(scale: f32) -> Self {
-        Plot {
-            scale,
-            origin: (screen_width() / 2.0, screen_height() / 2.0),
-        }
+        Plot { scale }
+    }
+
+    fn origin_x(&self) -> f32 {
+        (screen_width() / 2.0).floor()
+    }
+
+    fn origin_y(&self) -> f32 {
+        (screen_height() / 2.0).floor()
     }
 
     fn screen_x(&self, x: f32) -> f32 {
-        self.origin.0 + x * self.scale
+        self.origin_x() + x * self.scale
     }
 
     fn screen_y(&self, y: f32) -> f32 {
-        self.origin.1 + y * -self.scale
+        self.origin_y() + y * -self.scale
     }
 
     pub fn from_screen_point(&self, (x, y): (f32, f32)) -> (f32, f32) {
         (
-            (x - self.origin.0) / self.scale,
-            (y - self.origin.1) / -self.scale,
+            (x - self.origin_x()) / self.scale,
+            (y - self.origin_y()) / -self.scale,
         )
     }
 
     pub fn draw_axes(&self) {
         draw_line(
             0.0,
-            self.origin.1,
+            self.origin_y(),
             screen_width(),
-            self.origin.1,
+            self.origin_y(),
             1.0,
             DARKGRAY,
         );
         draw_line(
-            self.origin.0,
+            self.origin_x(),
             0.0,
-            self.origin.0,
+            self.origin_x(),
             screen_height(),
             1.0,
             DARKGRAY,
